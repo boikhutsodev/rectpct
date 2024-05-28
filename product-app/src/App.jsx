33c9bfb503e1b1405c5bd8f9e572e6714c2cd91f
Boikhutso Mokoka <boikhuso.m@gmail.com>
2024-05-28 14:58:13 +0200

    learning react

---
+++ product-app/src/App.jsx
@@ -0,0 +1,15 @@
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
+function App() {
+  return (
+    <div>
+      <Welcome name="Alice" />
+      <Welcome name="Bob" />
+      <Welcome name="Charlie" />
+    </div>
+  );
+}
+
+export default App;
