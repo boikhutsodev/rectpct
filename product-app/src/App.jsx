5e631cb92d06071584473cf4953cea99cdc92373
Boikhutso Mokoka <boikhuso.m@gmail.com>
2024-06-26 14:32:45 +0200

    Removed User component and replaced App component with Password component.

--- product-app/src/App.jsx
+++ product-app/src/App.jsx
@@ -1,25 +1,19 @@
-import User from "./User";
 import React from "react";
 
 // Watched 1:14:57
+const ValidPassword = () => <h1>Valid Password</h1>;
+const InvalidPassword = () => <h1>Invalid Password</h1>;
 
+const Password = ({ isValid }) => {
+  if (isValid) {
+    return <ValidPassword />;
+  }
+  return <InvalidPassword />;
+};
 function App() {
   return (
     <>
-      <User
-        img="https://images.pexels.com/photos/593655/pexels-photo-593655.jpeg?auto=compress&cs=tinysrgb&w=400"
-        name="Boikhutso WebDev"
-        age={18}
-        hobbies={["Coding ", "Reading ", "Sleeping ", "Recording "]}
-      >
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea tempora
-          repellendus officiis corrupti magni accusantium cum est error repellat
-          eos. Nihil aspernatur sapiente architecto accusamus. Dicta, temporibus
-          molestiae. Laboriosam, nihil.
-        </p>
-        <h1>Hello I am a children</h1>
-      </User>
+      <Password isValid={true} />
     </>
   );
 }
