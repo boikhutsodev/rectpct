d8ea1371cc0c9e12d1334751c3af3cd6d7bac971
Boikhutso Mokoka <boikhuso.m@gmail.com>
2024-06-26 15:44:02 +0200

    Removed styles object and inline styles from App.jsx, added import for index.css in main.jsx"

--- product-app/src/App.jsx
+++ product-app/src/App.jsx
@@ -2,25 +2,10 @@ import React from "react";
 
 // Watched 1:14:57
 
-const differentStyle = {
-  color: "blue",
-  fontSize: "50px",
-  textDecoration: "underline",
-  background: "lightGreen",
-};
-
 function App() {
   return (
     <>
-      <h1
-        style={{
-          color: differentStyle.color,
-          background: differentStyle.background,
-          fontSize: differentStyle.fontSize,
-        }}
-      >
-        Hello I am Boikhutso
-      </h1>
+      <h1 style={{ background: "lightgrey" }}>Hello I am Boikhutso</h1>
     </>
   );
 }
