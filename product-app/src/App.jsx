f4bf94dc6c759f809b2f2c8cbb30be343c0c07c9
Boikhutso Mokoka <boikhuso.m@gmail.com>
2024-06-26 16:54:40 +0200

    Added Bootstrap components and functionality to App.jsx, including a Navbar with login form, search form, and buttons with various variants.

--- product-app/src/App.jsx
+++ product-app/src/App.jsx
@@ -2,12 +2,68 @@ import React from "react";
 
 // Watched 1:14:57
 
+import Button from "react-bootstrap/Button";
+
+import Navbar from "react-bootstrap/Navbar";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+function NavBarLogin() {
+  return (
+    <Navbar className="bg-body-tertiary justify-content-between">
+      <Form inline>
+        <InputGroup>
+          <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+          <Form.Control
+            placeholder="Username"
+            aria-label="Username"
+            aria-describedby="basic-addon1"
+          />
+        </InputGroup>
+      </Form>
+      <Form inline>
+        <Row>
+          <Col xs="auto">
+            <Form.Control
+              type="text"
+              placeholder="Search"
+              className=" mr-sm-2"
+            />
+          </Col>
+          <Col xs="auto">
+            <Button type="submit">Submit</Button>
+          </Col>
+        </Row>
+      </Form>
+    </Navbar>
+  );
+}
+
+function Buttons() {
+  return (
+    <>
+      <Button variant="outline-primary">Primary</Button>{" "}
+      <Button variant="outline-secondary">Secondary</Button>{" "}
+      <Button variant="outline-success">Success</Button>{" "}
+      <Button variant="outline-warning">Warning</Button>{" "}
+      <Button variant="outline-danger">Danger</Button>{" "}
+      <Button variant="outline-info">Info</Button>{" "}
+      <Button variant="outline-light">Light</Button>{" "}
+      <Button variant="outline-dark">Dark</Button>
+    </>
+  );
+}
+
 function App() {
   return (
     <>
+      <NavBarLogin />
       <div className="container py-4 px-3 mx-auto">
-        <h1>Hello, Bootstrap and Vite!</h1>
-        <button className="btn btn-secondary">Primary button</button>
+        <h1>Hello, I am Boikhutso</h1>
+        <button className="btn btn-primary">Primary button</button>
+        <Buttons />
       </div>
     </>
   );
