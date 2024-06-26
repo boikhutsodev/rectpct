644cebdc83d50303d130e9916522cb771c2e42cc
Boikhutso Mokoka <boikhuso.m@gmail.com>
2024-06-26 09:52:26 +0200

    Added React import to App.jsx, modified User component to accept children prop, and added children elements to User component in App.jsx

--- product-app/src/App.jsx
+++ product-app/src/App.jsx
@@ -1,4 +1,5 @@
 import User from "./User";
+import React from "react";
 
 // Watched 1:14:57
 
@@ -10,7 +11,15 @@ function App() {
         name="Boikhutso WebDev"
         age={18}
         hobbies={["Coding ", "Reading ", "Sleeping ", "Recording "]}
-      />
+      >
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea tempora
+          repellendus officiis corrupti magni accusantium cum est error repellat
+          eos. Nihil aspernatur sapiente architecto accusamus. Dicta, temporibus
+          molestiae. Laboriosam, nihil.
+        </p>
+        <h1>Hello I am a children</h1>
+      </User>
     </>
   );
 }
--- product-app/src/User.jsx
+++ product-app/src/User.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
-const User = ({ img, name, hobbies }) => {
+const User = ({ img, name, hobbies, children }) => {
   return (
     <section>
       <img src={img} alt={img} />
       <h1>Name: {name}</h1>
       <h3>Hobbies: {hobbies}</h3>
+      {children}
     </section>
   );
 };
