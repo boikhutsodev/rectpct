5b9ef2be733a29b67afd549d1047f67e18ac1bc7
Boikhutso Mokoka <boikhuso.m@gmail.com>
2024-06-28 11:36:21 +0200

    Added Counter component with state and buttons to increment and decrement count number, and rendered it in App component.

--- product-app/src/App.jsx
+++ product-app/src/App.jsx
@@ -1,9 +1,32 @@
 import React from "react";
 
 // Watched 1:59:57
+import { useState } from "react";
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+  const subtract = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <>
+      <h1>{count}</h1>
+      <button onClick={increment}>+</button>
+      <button onClick={subtract}>-</button>
+    </>
+  );
+};
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Counter />
+    </>
+  );
 }
 
 export default App;
