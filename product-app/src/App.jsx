f130c118c68c24284670a600479734bc3f99ce3b
Boikhutso Mokoka <boikhuso.m@gmail.com>
2024-07-01 13:14:19 +0200

    Replaced movie list with form demo, updated state and event handlers.

--- product-app/src/App.jsx
+++ product-app/src/App.jsx
@@ -1,29 +1,26 @@
 import React from "react";
 
-// Watched 2:19:47
+// Watched 2:27:32
 import { useState } from "react";
 
 function App() {
-  const [movies, setMovies] = useState([
-    { id: 1, title: "Popy", ratings: 6 },
-    { id: 2, title: "Atlas", ratings: 9 },
-    { id: 3, title: "Hall pass", ratings: 4 },
-  ]);
+  const [username, setUsername] = useState("");
 
-  const handleClick = () => {
-    setMovies(
-      movies.map((m) =>
-        m.id === 3 ? { ...movies, title: "John Wick 4", ratings: 10 } : m
-      )
-    );
+  const handleChange = (event) => {
+    setUsername(event.target.value);
   };
-
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`You typed: ${username}`);
+    setUsername("");
+  };
   return (
     <>
-      {movies.map((movie) => (
-        <li key={Math.random()}>{movie.title}</li>
-      ))}
-      <button onClick={handleClick}>Change Name</button>
+      <h1>Form Demo</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={username} onChange={handleChange} />
+        <button>Submit</button>
+      </form>
     </>
   );
 }
