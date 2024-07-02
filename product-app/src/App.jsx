7964a8b08624867dbb5a6e82074d9317800901d6
Boikhutso Mokoka <boikhuso.m@gmail.com>
2024-07-02 15:53:55 +0200

    Added useRef hook, input element, and button to App component.

--- product-app/src/App.jsx
+++ product-app/src/App.jsx
@@ -1,9 +1,21 @@
 import React from "react";
-
+import { useRef } from "react";
 // Watched 2:44:28
 
 function App() {
-  return <></>;
+  const inputElement = useRef(null);
+
+  const focusInput = () => {
+    inputElement.current.focus();
+    inputElement.current.value = "BoiTso";
+  };
+
+  return (
+    <>
+      <input type="text" ref={inputElement} />
+      <button onClick={() => focusInput()}>Focus & Write BoiTso</button>
+    </>
+  );
 }
 
 export default App;
