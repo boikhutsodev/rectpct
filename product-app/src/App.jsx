3c0b192787eeff9e64453b0b6c6928ab3fe3157c
Boikhutso Mokoka <boikhuso.m@gmail.com>
2024-07-02 15:30:07 +0200

    Removed ComponentA, ComponentB, and ComponentC files; modified App.jsx to use useReducer and commented out reducer logic.

--- product-app/src/App.jsx
+++ product-app/src/App.jsx
@@ -1,27 +1,42 @@
 import React from "react";
-
+import { useReducer } from "react";
 // Watched 2:44:28
-import { createContext } from "react";
-
-import ComponentC from "./ComponentC";
-
-// Contex API
-export const Data = createContext();
-export const Data1 = createContext();
 
 function App() {
-  const name = "BoikhutsoDev";
-  const age = 25;
-  const title = "Wap Developer";
+  // const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <>
-      <Data.Provider value={name}>
-        <Data1.Provider value={title}>
-          <ComponentC />
-        </Data1.Provider>
-      </Data.Provider>
+      {/* <h1>{state.count}</h1>
+      <button onClick={() => dispatch({ type: "increment" })}>+</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>-</button>
+      <button onClick={() => dispatch({ type: "reset" })}>Reset</button> */}
     </>
   );
 }
+
+// const initialState = { count: 0 };
+
+// const reducer = (state, action) => {
+//   switch (action.type) {
+//     case "increment":
+//       return {
+//         ...state,
+//         count: state.count + 1,
+//       };
+//     case "decrement":
+//       return {
+//         ...state,
+//         count: state.count - 1,
+//       };
+//     case "reset":
+//       return {
+//         ...state,
+//         count: 0,
+//       };
+//     default:
+//       return state;
+//   }
+// };
 
 export default App;
